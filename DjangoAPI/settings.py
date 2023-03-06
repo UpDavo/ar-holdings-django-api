1c30ef3acc9db1bff66500cfd563090092d2b37e
Anthony <anthony.villegas@nodel.co>
2023-03-06 10:55:06 -0500

    update

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
     "ArHoldingsApp.apps.ArholdingsappConfig",

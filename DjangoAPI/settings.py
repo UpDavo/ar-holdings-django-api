747775f412a818ceeb18cce39a17f1e8eb6d5cc2
Anthony <anthony.villegas@nodel.co>
2023-03-06 10:47:33 -0500

    update

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = "DjangoAPI.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": env.str("DB_NAME"),
-        "USER": env.str("DB_USER"),
-        "PASSWORD": env.str("DB_PASSWORD"),
-        "HOST": env.str("DB_HOST"),
-        "PORT": env.str("DB_PORT"),
+        "NAME": "freedb_arholdings2",
+        "USER": "freedb_updavo2",
+        "PASSWORD": "4j%$KM!nbV6*9hW",
+        "HOST": "sql.freedb.tech",
+        "PORT": "3306",
     }
 }
 
